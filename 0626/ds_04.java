6b88c9c728692ce6275235015deddec3b419956a
GOLF666 <kean8760@gmail.com>
2025-06-29 23:34:33 +0800

    Create ds_04.java

---
+++ 0626/ds_04.java
@@ -0,0 +1,8 @@
+public class ds_04 {
+    public static void main(String[] args) {
+        int[] nums = {10, 20, 30, 40, 50};
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println("第" + (i+1) + "個數是：" + nums[i]);
+        }
+    }
+}
